c52af6167a5882b8185619a974e353ce56ccc690
Eugene <eugene.zolotkov@gmail.com>
2023-04-21 17:46:20 +0300

    Add dynamic client api

--- src/api/index.js
+++ src/api/index.js
@@ -1,51 +1,43 @@
 const transport = {}
 
-transport.http = (url) => (structure) => {
-  const api = {};
-  const services = Object.keys(structure);
+transport.http = url => structure => {
+  const api = {}
+  const services = Object.keys(structure)
   for (const name of services) {
-    api[name] = {};
-    const service = structure[name];
-    const methods = Object.keys(service);
+    api[name] = {}
+    const service = structure[name]
+    const methods = Object.keys(service)
     for (const method of methods) {
-      api[name][method] = ({ ...args }) =>
+      api[name][method] = ({ token = '', ...args } = {}) =>
         new Promise((resolve, reject) => {
           fetch(`${url}/api/${name}/${method}`, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ args }),
-          }).then((res) => {
-            if (res.status === 200 || res.status === 201) resolve(res.json());
-            else res.text().then((message) => {
-              reject(new Error(`${message}`));
-            });
-          });
-        });
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({ args })
+          }).then(res => {
+            if (res.status === 200 || res.status === 201) resolve(res.json())
+            else
+              res.text().then(message => {
+                reject(new Error(`${message}`))
+              })
+          })
+        })
     }
   }
-  
-  return Promise.resolve(api);
-};
 
-const scaffold = (url) => {
-  const protocol = 'http'; // url.startsWith('ws:') ? 'ws' : 'http';
-  return transport[protocol](url);
-};
+  return Promise.resolve(api)
+}
 
-export default async function apiMethods() {  
-  const result = await scaffold('http://localhost:8001')({
-    item: {
-      create: ['record'],
-      read: ['id'],
-    },
-    user: {
-      create: ['record'],
-      find: ['email'],
-    },
-    auth: {
-      register: ['id', 'password'],
-      login: ['email', 'password'],
-    }
-  });
-  return result
+const scaffold = url => transport['http'](url)
+
+export default async function apiMethods () {
+  const url = 'http://localhost:8001'
+  const systemApi = await scaffold(url)({ system: { load: [] } })
+  const systemResponse = await systemApi.system.load()
+  const structure = systemResponse.clientApi
+  const fullApi = await scaffold(url)(structure)
+  return fullApi
 }
