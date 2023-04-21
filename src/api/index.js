ddc941394a5897e4dacbd62bf7754f21e93a0d44
Eugene <eugene.zolotkov@gmail.com>
2023-04-21 18:15:37 +0300

    fix headers

--- src/api/index.js
+++ src/api/index.js
@@ -1,43 +1,49 @@
-const transport = {}
-
-transport.http = url => structure => {
-  const api = {}
-  const services = Object.keys(structure)
-  for (const name of services) {
-    api[name] = {}
-    const service = structure[name]
-    const methods = Object.keys(service)
-    for (const method of methods) {
-      api[name][method] = ({ token = '', ...args } = {}) =>
-        new Promise((resolve, reject) => {
-          fetch(`${url}/api/${name}/${method}`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${token}`
-            },
-            body: JSON.stringify({ args })
-          }).then(res => {
-            if (res.status === 200 || res.status === 201) resolve(res.json())
-            else
-              res.text().then(message => {
-                reject(new Error(`${message}`))
-              })
-          })
-        })
-    }
-  }
-
-  return Promise.resolve(api)
+const HEADERS = {
+  'Content-Type': 'application/json; charset=UTF-8',
+  'Connection': 'keep-alive',
+  'Keep-Alive': 'timeout=2',
+  'Transfer-Encoding': 'chunked'
 }
 
-const scaffold = url => transport['http'](url)
+const transport = {}
 
-export default async function apiMethods () {
+transport.http = (url) => (structure) => {
+  const api = {};
+  const services = Object.keys(structure);
+  for (const name of services) {
+    api[name] = {};
+    const service = structure[name];
+    const methods = Object.keys(service);
+    for (const method of methods) {
+      api[name][method] = ({ token, ...args } = {}) =>
+      {
+        if (token) HEADERS['Authorization'] = `Bearer ${token}`
+        return new Promise((resolve, reject) => {
+          fetch(`${url}/api/${name}/${method}`, {
+            method: 'POST',
+            headers: HEADERS,
+            body: JSON.stringify({ args }),
+          }).then((res) => {
+            if (res.status === 200 || res.status === 201) resolve(res.json());
+            else res.text().then((message) => {
+              reject(new Error(`${message}`));
+            });
+          });
+        });
+      }
+    }
+  }
+  
+  return Promise.resolve(api);
+};
+
+const scaffold = (url) => transport['http'](url);
+
+export default async function apiMethods() {
   const url = 'http://localhost:8001'
-  const systemApi = await scaffold(url)({ system: { load: [] } })
-  const systemResponse = await systemApi.system.load()
-  const structure = systemResponse.clientApi
-  const fullApi = await scaffold(url)(structure)
+  const systemApi = await scaffold(url)({ system: { load: [] }});
+  const systemResponse = await systemApi.system.load();
+  const structure = systemResponse.clientApi;
+  const fullApi = await scaffold(url)(structure);
   return fullApi
 }
